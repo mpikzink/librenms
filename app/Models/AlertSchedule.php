4822b4a6b73f7f161a7f90517592f2613c5c6f0f
Kevin Zink <kevin.zink@mpi-hd.mpg.de>
2026-04-29 09:27:32 +0200

    Fix Style CI

--- app/Models/AlertSchedule.php
+++ app/Models/AlertSchedule.php
@@ -33,7 +33,6 @@ use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 use LibreNMS\Enum\AlertScheduleStatus;
 use LibreNMS\Enum\MaintenanceBehavior;
